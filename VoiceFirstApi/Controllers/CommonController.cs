ceff7760a3586156cfc2e41fcc3400d7a7b9a0c0
prathaphari@notetech.com <prathaphari@notetech.com>
2024-12-17 17:46:41 +0530

    update

---
+++ VoiceFirstApi/Controllers/CommonController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VoiceFirstApi.Controllers
+{
+    [Route("api/common")]
+    [ApiController]
+    public class CommonController : ControllerBase
+    {
+    }
+}
